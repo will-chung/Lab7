331d89dfb52293a6063681469c98bf75a54713fa
will-chung <billchung6@gmail.com>
2021-05-14 01:08:29 -0700

    Complete part 1a

--- scripts/router.js
+++ scripts/router.js
@@ -2,10 +2,12 @@
 
 export const router = {};
 
+const defaultUrl = window.location;
+
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function() {
+router.setState = function(state) {
   /**
    * - There are three states that your SPA app will have
    *    1. The home page
@@ -35,4 +37,22 @@ router.setState = function() {
    *    1. You may add as many helper functions in this file as you like
    *    2. You may modify the parameters of setState() as much as you like
    */
+
+  document.querySelector('body').className = state.className;
+
+  let title = document.querySelector('header > h1');
+  if (state.className == 'settings') {
+    title.innerHTML = 'Settings';
+  } else if (state.className == 'single-entry') {
+    let entryPage = document.createElement('entry-page')
+    title.innerHTML = 'Entry ' + state.entryNumber;
+    document.querySelector('entry-page').remove();
+    document.querySelector('main').insertAdjacentElement('afterend', entryPage);
+    entryPage.entry = state.entry;
+  } else {
+    title.innerHTML = 'Journal Entries';
+  }
+
+  console.log(state.url);
+  history.pushState(state, '', state.url);
 }
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ const setState = router.setState;
 
 // Make sure you register your service worker here too
 
+let state;
+
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
@@ -12,7 +14,44 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        newPost.addEventListener('click', (event) => {
+          let entries = event.target.parentElement.children;
+          let entryNumber;
+          for (let i = 0; i < entries.length; i++) {
+            if (entries.item(i) == event.target)
+              entryNumber = i + 1;
+          }
+          console.log(`journal entry ${entryNumber} clicked`);
+          state = {
+            className: 'single-entry',
+            url: '/#entry' + entryNumber,
+            entryNumber: entryNumber,
+            entry: event.target.entry,
+          };
+          router.setState(state);
+        });
         document.querySelector('main').appendChild(newPost);
       });
     });
+});
+
+let title = document.querySelector('header > h1');
+let settings = document.querySelector('header > img');
+
+title.addEventListener('click', () => {
+  console.log('title clicked');
+  state = {
+    className: '',
+    url: '/',
+  }
+  router.setState(state);
+});
+
+settings.addEventListener('click', () => {
+  console.log('settings clicked');
+  state = {
+    className: 'settings',
+    url: '/#settings',
+  }
+  router.setState(state);
 });
